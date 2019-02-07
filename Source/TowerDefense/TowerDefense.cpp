6b0e37d9d4b89909ff4ed006638320c7da50fb25
Sudzu97 <sudzu97@gmail.com>
2019-02-07 13:30:22 -0500

    Initial commit

---
+++ Source/TowerDefense/TowerDefense.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "TowerDefense.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowerDefense, "TowerDefense" );
+
+DEFINE_LOG_CATEGORY(LogTowerDefense)
+ 
---
+++ Source/TowerDefense/TowerDefenseGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "TowerDefenseGameMode.h"
+#include "TowerDefensePlayerController.h"
+#include "TowerDefenseCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATowerDefenseGameMode::ATowerDefenseGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = ATowerDefensePlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
