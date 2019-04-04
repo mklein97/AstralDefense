533f5305e34d0b18dc40e4a9a0a0d4dff7bf748c
Matt <sudzu97@gmail.com>
2019-04-03 21:57:49 -0400

    Added death handling

--- Source/TowerDefense/AstralDefenseGameMode.h
+++ Source/TowerDefense/AstralDefenseGameMode.h
@@ -18,4 +18,12 @@ public:
 	AAstralDefenseGameMode();
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	void resetTimer();
+
+	FTimerHandle timer;
+private:
+	bool flag;
 };
--- Source/TowerDefense/TowerDefense.Build.cs
+++ Source/TowerDefense/TowerDefense.Build.cs
@@ -6,7 +6,10 @@ public class TowerDefense : ModuleRules
 {
 	public TowerDefense(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	    MinFilesUsingPrecompiledHeaderOverride = 1;
+	    bFasterWithoutUnity = true;
+
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule" });
     }
